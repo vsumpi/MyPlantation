63a74cb56b08238d9042a2a9e0f51517b9cd8dde
Varga Zsombor <61274133+vsumpi@users.noreply.github.com>
2024-01-27 20:59:43 +0100

    🔧Fixed mysql table and insertion.
    Signed-off-by: Varga Zsombor <61274133+vsumpi@users.noreply.github.com>

--- myplantation_app.py
+++ myplantation_app.py
@@ -10,49 +10,48 @@ import pymysql
 # Connect to the database
 
 connection = pymysql.connect(host='localhost',
-                             user='sumpi',
+                             user='pythonapi',
                              password='admin',
-                             database='db',
+                             database='myplantationdb',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-
+"""
 with connection:
     with connection.cursor() as cursor:
         # Create a new record
         sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-        cursor.execute(sql, ('sumpi@localhost', 'admin'))
-
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
+        cursor.execute(sql, ('pythonapi', 'admin'))
     connection.commit()
 
     with connection.cursor() as cursor:
         # Read a single record
         sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-        cursor.execute(sql, ('sumpi@localhost',))
+        cursor.execute(sql, ('pythonapi'))
         result = cursor.fetchone()
         print(result)
-
+"""
 
 
 #  Create table for user ➕ (multiuser support)
 def make():
-    cur.execute(table)
-    con.commit()
+    with connection.cursor() as cursor:
+        cursor.execute(table)
+        connection.commit()
 
 # Register a plant with 3 input values and insert into the user's table
 def registerNewPlant():
-    plantDate = input("Enter when the plant was planted:")
-    storageId = input("Enter which storage is it in: ")
-    plantType = input("Enter what plant is it:")
-    newPlant = f"INSERT INTO {username}(planted, storageid, type) VALUES('{plantDate}',{storageId},\"{plantType}\");"
-    cur.execute(newPlant)
-    con.commit()
+    with connection.cursor() as cursor:
+        plantDate = input("Enter when the plant was planted:")
+        storageId = input("Enter which storage is it in: ")
+        plantType = input("Enter what plant is it:")
+        newPlant = f"INSERT INTO {username}(planted, storageid, type) VALUES('{plantDate}','{storageId}','{plantType}');"
+        cursor.execute(newPlant)
+        connection.commit()
 
 
 print("Please enter your username:")
 username = input()
-table = f"CREATE TABLE IF NOT EXISTS {username} (ID INTEGER PRIMARY KEY AUTOINCREMENT, planted date, lastirrigation date, storageid integer, type text);"
+table = f"CREATE TABLE IF NOT EXISTS {username}(ID INTEGER PRIMARY KEY AUTO_INCREMENT, planted DATE, lastirrigation DATE, storageid INTEGER, type TEXT);"
 make()
 
 # Activities 🧩
@@ -61,3 +60,6 @@ registerNewPlant()
 #res = cur.execute("SELECT * FROM sumpi")
 #for item in res.fetchall():
 #    print(item)
+with connection.cursor() as cursor:
+    cursor.close()
+    connection.close()
